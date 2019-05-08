266c492b9a9244b45e09eda954e2533f55f97831
Nicolas Cannasse <ncannasse@gmail.com>
2019-05-08 17:36:18 +0200

    use default env (related to #52 and #53)

--- src/HLAdapter.hx
+++ src/HLAdapter.hx
@@ -257,8 +257,7 @@ class HLAdapter extends adapter.DebugSession {
 			}
 		}
 		// ALLUSERSPROFILE required to spawn correctly on Windows, see vshaxe/hashlink-debugger!51.
-		var env = Sys.systemName() == "Windows" ? { ALLUSERSPROFILE: Sys.environment()['ALLUSERSPROFILE']} : {};
-		proc = ChildProcess.spawn("hl", hlArgs, {env: env, cwd: args.cwd});
+		proc = ChildProcess.spawn("hl", hlArgs, {cwd: args.cwd});
 		proc.stdout.setEncoding('utf8');
 		var prev = "";
 		proc.stdout.on('data', function(buf) {
