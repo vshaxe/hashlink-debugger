6e7d3794c94704d71314a19457628dedb77f1eee
Yuxiao Mao <yuxiao.mao@outlook.com>
2024-04-18 15:29:19 +0200

    Add some hack for non blocking waitpid Linux

--- hldebug-wrapper/src/debug.c
+++ hldebug-wrapper/src/debug.c
@@ -25,6 +25,7 @@
 #	include <sys/wait.h>
 #	include <sys/user.h>
 #	include <signal.h>
+#	include <errno.h>
 #	define USE_PTRACE
 #endif
 
@@ -248,9 +249,18 @@ HL_API int hl_debug_wait( int pid, int *thread, int timeout ) {
 	return mdbg_session_wait(pid, thread, timeout);
 #	elif defined(USE_PTRACE)
 	int status;
-	int ret = waitpid(pid,&status,0);
+	// *** HACK ***
+	// usleep here is needed for a good result.
+	// Without it, waitpid can miss many stop event;
+	// With it, and more we wait, less we miss stop event.
+	usleep(100 * 1000);
+	int ret = waitpid(pid, &status, WNOHANG);
 	//printf("WAITPID=%X %X\n",ret,status);
 	*thread = ret;
+	if( ret == -1 && errno != EINTR )
+		return 3;
+	if( ret <= 0 )
+		return -1;
 	if( WIFEXITED(status) )
 		return 0;
 	if( WIFSTOPPED(status) ) {
